5712957e66cd5bb71984c88b13f8fddee6ffa184
dcastro4 <d.castro4@utp.edu.co>
2023-06-06 21:52:49 -0500

    Update keys.js
    Change the connection limit to 4

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,24 @@
+// LOCAL
+
+/*
 module.exports = {
     database: {
         host: 'localhost',
         user: 'root',
         password: '',
         database: 'bookstore'
+    }
+}
+*/
+
+
+// CLEVER
+module.exports = {
+    database: {
+        connectionLimit: 4,
+        host: 'buefdks9wmfn4oen9azj-mysql.services.clever-cloud.com',
+        user: 'uyvvyz7uy3adceho',
+        password: 'a49AHLIMdHMdyetRllsq',
+        database: 'buefdks9wmfn4oen9azj'
     }
 }
